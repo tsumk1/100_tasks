17db0e9e832fbef34e174d10e7e26e291bb7eb8c
tsumk1 <n76474534@gmail.com>
2025-12-10 19:39:03 +0200

    five

---
+++ five.cpp
@@ -0,0 +1,15 @@
+//The user enters a number n. Calculate n! (the factorial).
+#include <iostream>
+int main () {
+	int n;
+	std::cout<<"Enter number: "<<std::endl;
+	std::cin>> n;
+
+        int res = 1;
+	for(int i = 1; i<=n ; i++){
+	res *= i; // res = 1 , so res*=i = res*i
+	}
+	std::cout<<res<<std::endl;
+
+	return 0;
+}
