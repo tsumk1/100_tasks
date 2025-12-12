f2ac5b134ea23478c8661daee369e7135d349543
tsumk1 <n76474534@gmail.com>
2025-12-12 13:03:44 +0200

    ten

---
+++ 010.cpp
@@ -0,0 +1,11 @@
+//Read a name and greet the user.
+#include <iostream>
+#include <string>
+int main() {
+	std::string name;
+	std::cout<<"Enter your name:  "<<std::endl;
+	std::cin>> name; 
+
+	std::cout<<"Hello "<< name << "! Nice to meet you ;)"<<std::endl;
+return 0;
+}
