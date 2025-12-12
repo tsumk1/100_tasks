b363a3031ef2d7b086cfaeb4ea1b423f07a351f1
tsumk1 <n76474534@gmail.com>
2025-12-12 14:38:03 +0200

    dklsp

---
+++ 016.cpp
@@ -0,0 +1,16 @@
+//Read two numbers and print whether they are equal.
+#include <iostream>
+int main () {
+int a, b;
+std::cout<<"Enter first number: "<<std::endl;
+std::cin>> a;
+std::cout<< "Enter second number: "<<std::endl;
+std::cin>> b;
+if(a == b){
+	std::cout<<"The numbers is equal."<<std::endl;
+	return 0;
+}
+std::cout<<"The number isn't equal."<<std::endl;
+
+return 0;
+}
