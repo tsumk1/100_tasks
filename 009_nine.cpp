dc0810c6acac45f6f89b2b176a369d3928fccedc
tsumk1 <n76474534@gmail.com>
2025-12-12 12:45:12 +0200

    nine

---
+++ 009_nine.cpp
@@ -0,0 +1,15 @@
+//Find the remainder from division.
+#include <iostream>
+int main() {
+int a, b;
+std::cout<<"Enter dividend: "<<std::endl;
+std::cin>> a;
+std::cout<<"Enter divisor: "<<std::endl;
+std::cin>> b;
+
+int result = a%b;
+std::cout<<"Result: " << result<<std::endl;
+
+
+return 0; 
+}
