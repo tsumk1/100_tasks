8cedfd842305721d2255c2a9a70897d3f93df902
tsumk1 <n76474534@gmail.com>
2025-12-12 13:29:39 +0200

    twelve

---
+++ 012.cpp
@@ -0,0 +1,15 @@
+//Read an integer and print its cube.
+
+#include <iostream>
+int main () {
+int  a;
+std::cout<<"Enter number: "<<std::endl;
+std::cin >> a;
+ 
+int result = a*a*a;
+std::cout <<"Result: "<< result<<std::endl ;
+
+
+
+return 0;
+}
