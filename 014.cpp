7561fbf10f55ef7c1cb8cd406a6604b876677474
tsumk1 <n76474534@gmail.com>
2025-12-12 14:24:25 +0200

    Fourteen

---
+++ 014.cpp
@@ -0,0 +1,14 @@
+//Read a number and check if it is positive or negative.
+#include <iostream>
+int main () {
+int n;
+std::cout<<"Enter number: "<<std::endl;
+std::cin>> n;
+if(n < 0 && n != 0){
+	std::cout<<"The number is negative."<<std::endl;
+}else {
+	std:: cout<<"The number is positive."<<std::endl;
+
+}
+return 0;
+}
