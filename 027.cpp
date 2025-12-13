ddfbf4066b32250a6127376d5d2f7c1f54bda45a
tsumk1 <n76474534@gmail.com>
2025-12-13 19:40:57 +0200

    twenty-seven

---
+++ 027.cpp
@@ -0,0 +1,10 @@
+//Print all squares from 1² to 10².
+#include <iostream>
+int main (){
+	std::cout<<"Print all squares from 1² to 10²."<<std::endl;
+
+	
+for (int i = 1; i <= 10; i++)
+	std::cout<< i*i <<std::endl;
+return 0;
+}
