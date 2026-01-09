69464fc4abe5e2a586ab3b5c3294fbb15204170f
tsumi <n76474534@gmail.com>
2026-01-09 20:45:45 +0200

    UPDATE-four

--- 004_four.cpp
+++ 004_four.cpp
@@ -5,7 +5,11 @@ int main (){
 	std::cout<<"Enter number: "<<std::endl;
 	std::cin>> a;
 
-	if (a%2 
+	if (a%2==0){
+		std::cout<<"Yes, the number is even. "<<std::endl;
+	}else{
+		std::cout<<"No, the number isn`t even."<<std::endl;
+	}	
 return 0;
 
 
