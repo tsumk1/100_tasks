ae6c2a39c72cbf7cfeba93e4f43b86a8bfa8755c
tsumi <n76474534@gmail.com>
2026-01-12 12:54:03 +0200

    forty-nine

---
+++ 049.cpp
@@ -0,0 +1,12 @@
+//Functions (easy). Write a function that prints “Hello”.
+#include <iostream>
+
+void printHello() {
+    std::cout << "Hello World!" << std::endl; 
+}
+
+int main() {
+    printHello();  
+    return 0;
+}
+
