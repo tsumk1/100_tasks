f5d7ea47bf58a2dd3bc97d69b9612f5e0a6a13bb
tsumi <n76474534@gmail.com>
2026-02-21 21:04:20 +0200

    seventy

---
+++ 070.cpp
@@ -0,0 +1,18 @@
+//Read a number (1–7) and print the weekday name. But using arrays.
+#include <iostream>
+#include <string>
+int main(){
+    int day;
+    std::string days[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    std::cout<<"Enter the day number: ";
+    std::cin>> day;
+    std::cout<<"-------------------------------------"<<std::endl;
+
+    if(day>=1 && day<=7){
+        std::cout<<days[day - 1]<<std::endl;
+    }else{
+          std::cout<<"There are only 7 days in a week."<<std::endl;
+    }
+
+    return 0;
+}
